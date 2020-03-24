4560cfe9deef5ab1b1735026648b968b7c489ba7
Masaki <interceptor-2q7x6@live.jp>
2020-03-24 15:28:51 +0000

    json add

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const messages = [
-    {
-        id: 1,
-        name: "Masaki",
-        content: "I love to play badminton.",
-        read: false
-    },
-    {
-        id: 2,
-        name: "Bill Gates",
-        content: "Success is a lousy teacher. It seduces smart people into thinking they can’t lose.",
-        read: false
-    },
-    {
-        id: 3,
-        name: "Steve Jobs",
-        content: "Design is not just what it looks like and feels like. Design is how it works.",
-        read: true
-    }
-];
+let messages = [];
+let id = 1;
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('The app is running!'));
+app.get('/', (req, res) => {
+    res.send('The app is running!')
+});
 
-app.get('/messages', (req, res) => res.json(messages));
+app.get('/message', (req, res) => {
+    res.json(messages)
+});
 
 // ~/messages/2
 app.get('/messages/:id', (req, res) => {
@@ -43,6 +31,19 @@ app.get('/messages/:id', (req, res) => {
     if(!flag) {
         res.send('Can not find any messages with ID');
     };
+});
+
+app.get('/messages', (req, res) => {
+    let body = req.body;
+    let new_message = {
+        id: id++,
+        name: body.name,
+        content: body.content,
+        read: body.read
+    }
+
+    messages.push(new_message);
+    res.send('New message added!');
 });
 
 app.listen(port, () => {
